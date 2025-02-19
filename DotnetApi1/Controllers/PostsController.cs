ad7824de1b119265e9e6a2eb99c7c4b83d7f21f1
xmern <presleyrichnkume@gmail.com>
2025-02-18 16:09:22 -0800

    first commit

---
+++ DotnetApi1/Controllers/PostsController.cs
@@ -0,0 +1,50 @@
+﻿using DotnetApi1.Models;
+using DotnetApi1.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace DotnetApi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly PostService _postService;
+        public PostsController() { 
+            _postService = new PostService();
+        }
+        [HttpGet]
+        [Route("GetAllPosts")]
+        public ActionResult GetPost()
+        {
+            //_postService.GetAllPosts();
+            return Ok(_postService.GetAllPosts());
+        }
+        
+        
+        [HttpGet]
+        [Route("GetPostById/{id}")]
+        public ActionResult GetPostById(int id) {
+            Post post = _postService.GetPost(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+        
+        
+        [HttpPut]
+        [Route("CreatePost")]
+        public ActionResult CreatePost(Post post)
+        {
+            _postService.CreatePost(post);
+            return CreatedAtAction(nameof(GetPost), new {id = post.Id}, post);
+        }
+
+
+
+    }
+}
---
+++ DotnetApi1/Services/PostService.cs
@@ -0,0 +1,24 @@
+﻿using DotnetApi1.Models;
+
+namespace DotnetApi1.Services
+{
+    public class PostService
+    {
+        private static readonly List<Post> AllPosts = new List<Post>();
+        private static readonly Post[] AllPostsArr = new Post[50];
+
+        public void CreatePost(Post post)
+        {
+            AllPosts.Add(post);
+        }
+        public Post GetPost(int id)
+        {
+            Post thePost = AllPosts.FirstOrDefault(x => x.Id == id);
+            return thePost;
+        }
+        public List<Post> GetAllPosts()
+        {
+            return AllPosts;
+        }
+    }
+}
